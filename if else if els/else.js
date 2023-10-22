6cb2dd069e1d4f5fffc130af69af3bd27de7071a
Rabia Hameed <nrsuriya093@gmail.com>
2023-10-22 03:44:49 -0700

    first commit

---
+++ if else if els/else.js
@@ -0,0 +1,61 @@
+// Task1
+
+
+// Task 2
+// var num1 = prompt("Enter the first number: ");
+// var num2 = prompt("Enter the second number: ");
+
+// if (num1 === num2) {
+//   alert("The two numbers are equal.");
+// } else {
+//   if (num1 > num2) {
+//     alert("The larger number is " + num1 );
+//   } else {
+//     alert("The larger number is " + num2 );
+//   }
+// }
+
+// Task 3
+// var number = +prompt("Enter a number: ");
+
+// if (number > 0) {
+//   alert("The number is positive.");
+// } else if (number < 0) {
+//   alert("The number is negative.");
+// } else {
+//   alert("The number is zero.");
+// }
+
+// Task 4
+// var vowel = prompt("Enter a letter");
+
+// if(vowel == "a" || vowel == "e" || vowel == "i" || vowel == "o" || vowel == "u"){
+//     alert("Vowel")
+// }
+// else{
+//     alert("False")
+// }
+
+// Task 5
+// var correctPassword = "password";
+// var userPassword = prompt("Enter your password: ");
+
+// if (userPassword === "") {
+//   alert("Please enter your password.");
+// } else if (userPassword === correctPassword) {
+//   alert("Correct! The password you entered matches the original password.");
+// } else {
+//   alert("Incorrect password.");
+// }
+
+// Task 6
+// var greeting;
+// var hour = 13;
+
+// if (hour < 18) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+
+// Task 7
---
+++ user.js
@@ -0,0 +1,236 @@
+// Task 1
+// var city =prompt("Enter your city Name");
+// if(city =="karachi"){
+//     alert("Welcom to city of lights")
+// }
+// Task 2
+// var gender=prompt("Whats your Gender");
+// if(gender = "Male"){
+//     alert("Good Morning Sir");
+// }
+// else if(gender = "Female"){
+//     alert("Good Morning Ma'am");
+// }
+// Task 3
+// var signalColor=prompt("Enter signal colors");
+// if(signalColor == "Red"){
+//     alert("Must Stop")
+// }
+// else if(signalColor == "yellow"){
+//     alert("Ready to move")
+// }
+// else if(signalColor == "green"){
+//     alert("Move now")
+// }
+// Task 4
+// var fuel =+prompt("How many liters of fuel")
+// if(fuel <= "0.25"){
+//     alert("Please refill the fuel in your car")
+// }
+// Task 5
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+// TAsk 6
+// var heading = document.createElement("h2");
+// heading.innerHTML = "Mark Sheet";
+// document.body.appendChild(heading)
+
+// var totalNumbers = 300;
+// var Maths = +prompt("Enter your Maths marks");
+// var English = +prompt("Enter your English marks");
+// var Urdu = +prompt("Enter your Urdu marks");
+// var marks = Maths + English + Urdu;
+// var percentage = marks / totalNumbers * 100;
+
+// if(percentage >= 80){
+//     var para = document.createElement("p");
+//     para.innerHTML = "Total Marks: " + totalNumbers;
+//     document.body.appendChild(para)
+
+//     var para2 = document.createElement("p");
+//     para2.innerHTML = "Obtained Marks: " + marks;
+//     document.body.appendChild(para2)
+
+//     var para3 = document.createElement("p");
+//     para3.innerHTML = "Percentage: " + percentage + "%";
+//     document.body.appendChild(para3)
+
+//     var para4 = document.createElement("p");
+//     para4.innerHTML = "Grade: A-one";
+//     document.body.appendChild(para4)
+
+//     var para5 = document.createElement("p");
+//     para5.innerHTML = "Remarks: Excellent";
+//     document.body.appendChild(para5)
+// }
+
+// else if(percentage >= 70){
+//     var para = document.createElement("p");
+//     para.innerHTML = "Total Marks: " + totalNumbers;
+//     document.body.appendChild(para)
+
+//     var para2 = document.createElement("p");
+//     para2.innerHTML = "Obtained Marks: " + marks;
+//     document.body.appendChild(para2)
+
+//     var para3 = document.createElement("p");
+//     para3.innerHTML = "Percentage: " + percentage + "%";
+//     document.body.appendChild(para3)
+
+//     var para4 = document.createElement("p");
+//     para4.innerHTML = "Grade: A";
+//     document.body.appendChild(para4)
+
+//     var para5 = document.createElement("p");
+//     para5.innerHTML = "Remarks: Good";
+//     document.body.appendChild(para5)
+// }
+
+// else if(percentage >= 60){
+//     var para = document.createElement("p");
+//     para.innerHTML = "Total Marks: " + totalNumbers;
+//     document.body.appendChild(para)
+
+//     var para2 = document.createElement("p");
+//     para2.innerHTML = "Obtained Marks: " + marks;
+//     document.body.appendChild(para2)
+
+//     var para3 = document.createElement("p");
+//     para3.innerHTML = "Percentage: " + percentage + "%";
+//     document.body.appendChild(para3)
+
+//     var para4 = document.createElement("p");
+//     para4.innerHTML = "Grade: B";
+//     document.body.appendChild(para4)
+
+//     var para5 = document.createElement("p");
+//     para5.innerHTML = "Remarks: You need to improve";
+//     document.body.appendChild(para5)
+// }
+
+// else if(percentage < 60){
+//     var para = document.createElement("p");
+//     para.innerHTML = "Total Marks: " + totalNumbers;
+//     document.body.appendChild(para)
+
+//     var para2 = document.createElement("p");
+//     para2.innerHTML = "Obtained Marks: " + marks;
+//     document.body.appendChild(para2)
+
+//     var para3 = document.createElement("p");
+//     para3.innerHTML = "Percentage: " + percentage + "%";
+//     document.body.appendChild(para3)
+
+//     var para4 = document.createElement("p");
+//     para4.innerHTML = "Grade: Fail";
+//     document.body.appendChild(para4)
+
+//     var para5 = document.createElement("p");
+//     para5.innerHTML = "Remarks: Sorry";
+//     document.body.appendChild(para5)
+// }
+
+// Task 7
+// var number = 8;
+// var enterYourNumber = prompt("Guess the correct number between 1 and 10");
+
+// if(enterYourNumber == number){
+//     alert("Bingo! Correct answer")
+// }
+// else if(enterYourNumber == number + 1){
+//     alert("Close enough to the correct answer")
+// }
+
+// Task 9
+// var number = prompt("Enter your number");
+// if(number % 2 == 0){
+//     alert(number + " Even number")
+// }
+// else{
+//     alert(number + " Odd number")
+// }
+
+// Task 10
+// var temp = prompt("Enter temperature");
+
+// if (temp > 40) {
+//   alert("It's too hot outside!");
+// } else if (temp > 30) {
+//   alert("The weather today is normal.");
+// } else if (temp > 20) {
+//   alert("Today's weather is cool.");
+// } else if (temperature > 10) {
+//   alert("OMG! Today's weather is so cool!");
+// }
+
+// Task 11
+// var num1 = +prompt("Enter first number");
+// var num2 = +prompt("Enter second number");
+// var operator = prompt("Enter opertaor","+, -, *, /")
+
+// if(operator == "+"){
+//     alert(num1 + num2)
+// }
+// else if(operator == "-"){
+//     alert(num1 - num2)
+// }
+// else if(operator == "*"){
+//     alert(num1 * num2)
+// }
+// else if(operator == "/"){
+//     alert(num1 / num2)
+// }
+// else{
+//     alert("Something is wrong")
+// }
+
+
+
+
+
+
+
+
+
+
+
